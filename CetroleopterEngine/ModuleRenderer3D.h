35191ad7404d2e179426dbaeaf5985b580e485da
mrmile <alvaro.milla02@gmail.com>
2022-10-04 23:27:41 +0200

    Revert "Checkpoint Before Crash"
    This reverts commit 1336780551927a6cad0e531e7346306c9eca5a39.

--- CetroleopterEngine/ModuleRenderer3D.h
+++ CetroleopterEngine/ModuleRenderer3D.h
@@ -5,10 +5,8 @@
 #include "Globals.h"
 #include "glmath.h"
 #include "Light.h"
-#include <MathGeoLib.h>
 
 #define MAX_LIGHTS 10
-#define GLVertexDD(idx) {float3& v = vertices[*idx - 1]; glVertex3f(v.x, v.y, v.z);}
 
 class ModuleRenderer3D : public Module
 {
@@ -29,109 +27,6 @@ public:
 	SDL_GLContext context;
 	mat3x3 NormalMatrix;
 	mat4x4 ModelMatrix, ViewMatrix, ProjectionMatrix;
-
-	float3 vertices[8] = {
-	{-1., -1., -1.}, {1., -1., -1.},
-	{-1., 1., -1.}, {1., 1., -1.},
-	{-1., -1., 1.}, {1., -1., 1.},
-	{-1., 1., 1.}, {1., 1., 1.}
-	};
-
-	int indices[36] = {
-		5,6,8,	5,8,7,
-		4,2,1,	1,3,4,
-		4,3,8,	3,7,8,
-		2,6,1,	1,6,5,
-		1,7,3,	1,5,7,
-		2,4,8,	2,8,6
-	};
-
-	float3 colors[6] = {
-		{1., 1., 0.}, {1., 0., 1.}, {0., 1., 1.},
-		{1., 0., 0.}, {0., 1., 0.}, {0., 0., 1.}
-	};
-
-
-	void DDCube_VecIndices() {
-		glBegin(GL_TRIANGLES);
-		int col = -1;
-		for (int i = 0; i < sizeof(indices) / sizeof(int); ++i) {
-			if (i % 6 == 0) {
-				++col;
-				glColor3f(colors[col].x, colors[col].y, colors[col].z);
-			}
-			GLVertexDD(&indices[i]);
-		}
-		glEnd();
-	}
-
-	void DDCube_BadIndices()
-	{
-		glBegin(GL_TRIANGLES);
-		int* idx = indices;
-
-		// Front Face
-		glColor3f(1., 1., 0.);
-		GLVertexDD(idx); idx++; // 5
-		GLVertexDD(idx); idx++; // 6
-		GLVertexDD(idx); idx++; // 8
-
-		GLVertexDD(idx); idx++; // 5
-		GLVertexDD(idx); idx++; // 8
-		GLVertexDD(idx); idx++; // 7
-
-		// Back Fave
-		glColor3f(1., 0., 1.);
-		GLVertexDD(idx); idx++; // 4
-		GLVertexDD(idx); idx++; // 2
-		GLVertexDD(idx); idx++; // 1
-
-		GLVertexDD(idx); idx++; // 1
-		GLVertexDD(idx); idx++; // 3
-		GLVertexDD(idx); idx++; // 4
-
-		// Top Face
-		glColor3f(0., 1., 1.);
-		GLVertexDD(idx); idx++; // 4
-		GLVertexDD(idx); idx++; // 3
-		GLVertexDD(idx); idx++; // 8
-
-		GLVertexDD(idx); idx++; // 3
-		GLVertexDD(idx); idx++; // 7
-		GLVertexDD(idx); idx++; // 8
-
-		// Bottom Face
-		glColor3f(1., 0., 0.);
-		GLVertexDD(idx); idx++; // 2
-		GLVertexDD(idx); idx++; // 6
-		GLVertexDD(idx); idx++; // 1
-
-		GLVertexDD(idx); idx++; // 1
-		GLVertexDD(idx); idx++; // 6
-		GLVertexDD(idx); idx++; // 5
-
-		// SideL / TriT
-		glColor3f(0., 1., 0.);
-		GLVertexDD(idx); idx++; // 1
-		GLVertexDD(idx); idx++; // 7
-		GLVertexDD(idx); idx++; // 3
-
-		GLVertexDD(idx); idx++; // 1
-		GLVertexDD(idx); idx++; // 5
-		GLVertexDD(idx); idx++; // 7
-
-		// SideR / TriT
-		glColor3f(0., 0., 1.);
-		GLVertexDD(idx); idx++; // 2
-		GLVertexDD(idx); idx++; // 4
-		GLVertexDD(idx); idx++; // 8
-
-		GLVertexDD(idx); idx++; // 2
-		GLVertexDD(idx); idx++; // 8
-		GLVertexDD(idx); idx++; // 6
-
-		glEnd();
-	}
 };
 
 #endif // !_MODULE_RENDERER_H_
