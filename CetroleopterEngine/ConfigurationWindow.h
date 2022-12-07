e0500bbdc558f57eaf8b152201915c0d32647a99
mrmile <alvaro.milla02@gmail.com>
2022-12-07 22:14:50 +0100

    Added play/pause button

--- CetroleopterEngine/ConfigurationWindow.h
+++ CetroleopterEngine/ConfigurationWindow.h
@@ -33,7 +33,8 @@ private:
 	bool AnotherHeader();
 	bool FPSHeader();
 	bool HardwareHeader();
-	
+	bool PlayPauseHeader();
+
 
 private:
 
@@ -53,8 +54,8 @@ private:
 	float fpsData[MAX_HISTOGRAM_SIZE];
 	float msData[MAX_HISTOGRAM_SIZE];
 
-	
-
+	uint sceneTimer = 0;
+	bool counterON = false;
 };
 
 #endif // !_CONFIGURATIONWINDOW_H_
