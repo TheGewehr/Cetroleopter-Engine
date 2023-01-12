a546102166db89c323b7568338c225a186d7e57a
mrmile <alvaro.milla02@gmail.com>
2023-01-12 20:46:39 +0100

    Initialized audio module components
    Da todo error y está copiado del tutorial que viene con la librería así que no sé.

--- CetroleopterEngine/ModuleAudio.cpp
+++ CetroleopterEngine/ModuleAudio.cpp
@@ -5,6 +5,16 @@
 #include "ModuleCameraComponent.h"
 #include "ModuleTransformComponent.h"
 
+#include <AkMemoryMgr.h>                  // Memory Manager interface
+#include <AkModule.h>                     // Default memory manager
+
+#include <IAkStreamMgr.h>                 // Streaming Manager
+#include <AkPlatformFuncs.h>                    // Thread defines
+#include <AkFilePackageLowLevelIOBlocking.h>                    // Sample low-level I/O implementation
+#include <AkSoundEngine.h>                // Sound engine
+#include <AkMusicEngine.h>                // Music Engine
+#include <AkSpatialAudio.h>              // Spatial Audio
+
 ModuleAudio::ModuleAudio(bool start_enabled) : Module(start_enabled)
 {
 	
@@ -18,15 +28,84 @@ bool ModuleAudio::Init()
 {
 	LOG("Initializing ModuleAudio");
 
-	// Initializing default memory manager
-	//AkMemSettings memSettings;
-	//memSettings.uMaxNumPools = 20;
-	//
-	//if (AK::MemoryMgr::Init(&memSettings) != AK_Success)
-	//{
-	//	assert(!"Could not create the memory manager.");
-	//	return false;
-	//}
+	AkMemSettings memSettings;
+	AK::MemoryMgr::GetDefaultSettings(memSettings);
+
+	if (AK::MemoryMgr::Init(&memSettings) != AK_Success)
+	{
+		assert(!"Could not create the memory manager.");
+		return false;
+	}
+
+    AkStreamMgrSettings stmSettings;
+    AK::StreamMgr::GetDefaultSettings(stmSettings);
+
+    // Customize the Stream Manager settings here.
+
+    if (!AK::StreamMgr::Create(stmSettings))
+    {
+        assert(!"Could not create the Streaming Manager");
+        return false;
+    }
+
+    //
+    // Create a streaming device with blocking low-level I/O handshaking.
+    // Note that you can override the default low-level I/O module with your own. 
+    //
+    AkDeviceSettings deviceSettings;
+    AK::StreamMgr::GetDefaultDeviceSettings(deviceSettings);
+
+    // Customize the streaming device settings here.
+
+    // CAkFilePackageLowLevelIOBlocking::Init() creates a streaming device
+    // in the Stream Manager, and registers itself as the File Location Resolver.
+    if (g_lowLevelIO.Init(deviceSettings) != AK_Success)
+    {
+        assert(!"Could not create the streaming device and Low-Level I/O system");
+        return false;
+    }
+
+    //
+    // Create the Sound Engine
+    // Using default initialization parameters
+    //
+
+    AkInitSettings initSettings;
+    AkPlatformInitSettings platformInitSettings;
+    AK::SoundEngine::GetDefaultInitSettings(initSettings);
+    AK::SoundEngine::GetDefaultPlatformInitSettings(platformInitSettings);
+
+    if (AK::SoundEngine::Init(&initSettings, &platformInitSettings) != AK_Success)
+    {
+        assert(!"Could not initialize the Sound Engine.");
+        return false;
+    }
+
+    //
+    // Initialize the music engine
+    // Using default initialization parameters
+    //
+
+    AkMusicSettings musicInit;
+    AK::MusicEngine::GetDefaultInitSettings(musicInit);
+
+    if (AK::MusicEngine::Init(&musicInit) != AK_Success)
+    {
+        assert(!"Could not initialize the Music Engine.");
+        return false;
+    }
+
+    //
+    // Initialize Spatial Audio
+    // Using default initialization parameters
+    //
+
+    AkSpatialAudioInitSettings settings; // The constructor fills AkSpatialAudioInitSettings with the recommended default settings. 
+    if (AK::SpatialAudio::Init(&settings) != AK_Success)
+    {
+        assert(!"Could not initialize the Spatial Audio.");
+        return false;
+    }
 
 	return true;
 }
