87b346f777321369fa6312c58b72acd15b45fbfa
TheGewehr <alexandre07979@gmail.com>
2023-01-16 19:32:16 +0100

    Hotfix

--- CetroleopterEngine/Game/Library/Sounds/Wwise_IDs.h
+++ CetroleopterEngine/Game/Library/Sounds/Wwise_IDs.h
@@ -26,6 +26,7 @@ namespace AK
 
     namespace GAME_PARAMETERS
     {
+        static const AkUniqueID NEW_GAME_PARAMETER = 3671138082U;
         static const AkUniqueID SS_AIR_FEAR = 1351367891U;
         static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
         static const AkUniqueID SS_AIR_FURY = 1029930033U;
